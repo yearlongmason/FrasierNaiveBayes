3b0cfd738a23f3782fd8b7edca190af06015371c
yearlongmason <mdudelee@gmail.com>
2025-01-04 03:28:24 -0500

    Added train test split and tokenization function

--- naiveBayes.py
+++ naiveBayes.py
@@ -4,6 +4,7 @@
 
 import csv
 from collections import defaultdict
+from random import shuffle
 
 class NaiveBayes:
     def __init__(self):
@@ -25,18 +26,34 @@ class NaiveBayes:
 
         return self.characterLines
 
-    def trainTestSplit(testSize: int=.25) -> tuple[dict[str, list[str]]]:
+    def trainTestSplit(self, testSize: int=.25) -> tuple[dict[str, list[str]]]:
         """This function splits and returns data into testing and training sets"""
         # Creates two copies of the original character lines dictionary
         train = defaultdict(list)
         test = defaultdict(list)
         # Fill each dictionary with the respective amount of lines based on the testSize parameter
+        for character in self.characterLines.keys():
+            shuffledLines = self.characterLines[character].copy()
+            shuffle(shuffledLines)
+            # Get the split point (amount of data we're taking for the training data)
+            splitPoint = int(len(shuffledLines) * (1 - testSize))
+            
+            # Set the training and testing lines for the character
+            train[character] = shuffledLines[:splitPoint]
+            test[character] = shuffledLines[splitPoint:]
+
         return train, test
 
-    def getWordCounts() -> dict[str, dict[str, int]]:
+    def getWordCounts(self, data) -> dict[str, dict[str, int]]:
         """This function is essentially supposed to be a tokenization function
         Returns a dictionary with each character's wordCounts"""
-        pass
+        tokenCounts = defaultdict(lambda: defaultdict(int))
+        for character in data.keys():
+            for line in data[character]:
+                for word in line.split(" "):
+                    tokenCounts[character][word] += 1
+            
+        return tokenCounts
 
     def getProbabilities(line: str) -> dict[str, float]:
         """This returns a dictionary of the probability for each character"""
@@ -55,4 +72,4 @@ if __name__ == "__main__":
     # getData file path: "./data/cleanedTranscript.csv"
     frasierNBModel = NaiveBayes()
     frasierNBModel.getData(filePath="./data/cleanedTranscript.csv")
-    print(frasierNBModel.trainTestSplit())
+    train, test = frasierNBModel.trainTestSplit()
