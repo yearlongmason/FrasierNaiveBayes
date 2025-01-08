dda72def4d7946efd4f4d55e8c4566c1f51185d0
yearlongmason <mdudelee@gmail.com>
2025-01-08 00:45:57 -0500

    Optimized traintestsplit function with minWords parameter

--- naiveBayes.py
+++ naiveBayes.py
@@ -13,24 +13,24 @@ class NaiveBayes:
     def getData(self, filePath: str, normalizeLens: bool=False, minWords: int=None, maxWords: int=None) -> dict[str, list[str]]:
         """Sets self.characterLines equal to data from specified file in format: 
         {characterName: [allCharacter lines], ... }
-        This function should also return characterLines"""
-        # Define the word range
+        This function also returns characterLines"""
+        # Define the word range (default max is 9999 because it will never exceed that)
         wordRange = (0 if not minWords else minWords, 9999 if not maxWords else maxWords)
 
         self.characterLines = defaultdict(list)
         with open(filePath, newline='', encoding="utf8") as file:
-            # Loop through each csv row
             for row in csv.reader(file, delimiter=","):
                 # If the character is a main character, add their line to their respective list of lines
                 if row[14] == "main":
                     character = row[0]
                     line = row[1]
 
-                    # Account for minWords and maxWords parameters
-                    if len(line.split()) >= wordRange[0] and len(line.split()) <= wordRange[1]:
+                    # Make sure the line is within the valid range
+                    numWords = len(line.split())
+                    if numWords >= wordRange[0] and numWords <= wordRange[1]:
                         self.characterLines[character].append(line)
 
-        # Make all characters have the same number of lines
+        # Make all characters have the same number of lines if the user chooses
         if normalizeLens:
             minLen = min([len(lines) for lines in self.characterLines.values()])
             for character in self.characterLines.keys():
@@ -39,7 +39,7 @@ class NaiveBayes:
 
         return self.characterLines
 
-    def trainTestSplit(self, testSize: float=.25, strictTestSize: int=None) -> tuple[dict[str, list[str]]]:
+    def trainTestSplit(self, testSize: float=.25, strictTestSize: int=None, minWords: int=None) -> tuple[dict[str, list[str]]]:
         """This function splits and returns data into testing and training sets"""
         # Creates two copies of the original character lines dictionary
         train = defaultdict(list)
@@ -51,14 +51,23 @@ class NaiveBayes:
             # Get the split point (amount of data we're taking for the training data)
             splitPoint = int(len(shuffledLines) * (1 - testSize))
             
-
-            # Get all lines with valid length and get subset of it
-            # Set train equal to the rest of the valid lengths and the invalid lengths!
             # Set the training and testing lines for the character
             if strictTestSize:
                 splitPoint = len(shuffledLines) - strictTestSize
-            train[character] = shuffledLines[:splitPoint]
-            test[character] = shuffledLines[splitPoint:]
+
+            # Get all lines with valid length and get subset of it
+            # Set train equal to the rest of the valid lengths and the invalid lengths
+            if minWords:
+                validTestLines = [line for line in shuffledLines if len(line.split()) >= minWords]
+                invalidTestLines = [line for line in shuffledLines if len(line.split()) < minWords]
+                splitPoint = len(validTestLines) - strictTestSize
+                train[character] = validTestLines[:splitPoint]
+                test[character] = validTestLines[splitPoint:]
+                train[character].extend(invalidTestLines)
+            # If minWords not specefied then just split data as normal
+            else:
+                train[character] = shuffledLines[:splitPoint]
+                test[character] = shuffledLines[splitPoint:]
 
         return train, test
 
@@ -89,13 +98,13 @@ class NaiveBayes:
             # Multiply the current probability by the new word's probability (updating our beliefs based on new data) 
             for word in line.split():
                 probabilies[character] *= ((tokenizedLines[character][word] + 1) / totalNumWords)
-                # Good for debugging / seeing what's going on:
+                # Good for debugging / seeing what's going on (probabilities changing):
                 #print(f"{word}: {tokenizedLines[character][word] + 1} | New probability = {probabilies[character]}")
 
         return probabilies
 
     def classifyLine(self, train: dict[str, list[str]], line: str, wordCounts) -> str:
-        """Using the helper functions, this will take in a line and classify it as a specific character"""
+        """Using the helper functions, take in a line and classify it as a specific character"""
         return max(self.getProbabilities(train, line, wordCounts).items(), key=lambda pair: pair[1])
 
     def scoreCharacter(self, character: str, train: dict[str, list[str]], test: dict[str, list[str]], wordCounts) -> float:
@@ -104,7 +113,6 @@ class NaiveBayes:
         for i, line in enumerate(test[character]):
             if self.classifyLine(train, line, wordCounts)[0] == character:
                 numRight += 1
-                # print(f"{i}: {numRight}") # Good for checking progress
 
         return numRight/len(test[character])
 
@@ -113,12 +121,7 @@ def runFrasierNB(minWordsTest: int=None, minWordsAll: int=None, strictTestSize: 
     """This function is used to run a test, it returns nothing but prints out each character's score"""
     frasierNBModel = NaiveBayes()
     frasierNBModel.getData(filePath="./data/cleanedTranscript.csv", minWords=minWordsAll)
-    train, test = frasierNBModel.trainTestSplit(testSize=.25, strictTestSize=strictTestSize)
-
-    if minWordsTest:
-        # Remove lines less than or equal to minWordsTest
-        for character in test.keys():
-            test[character] = [line for line in test[character] if len(line.split()) >= minWordsTest]
+    train, test = frasierNBModel.trainTestSplit(testSize=.25, strictTestSize=strictTestSize, minWords=minWordsTest)
 
     # Get number of lines per character
     if printLinesPerCharacter:
@@ -133,7 +136,7 @@ def runFrasierNB(minWordsTest: int=None, minWordsAll: int=None, strictTestSize: 
 
 if __name__ == "__main__":
     # Run through multiple tests using different minWord values
-    for minWords in [5, 10, 15, 20, 25]:
+    for minWords in [0, 5, 10, 15, 20, 25]:
         print(f"Testing with lines that have a minimum of {minWords} words:")
-        runFrasierNB(strictTestSize=500, minWordsTest=minWords)
+        runFrasierNB(strictTestSize=250, minWordsTest=minWords, printLinesPerCharacter=False)
         print()
